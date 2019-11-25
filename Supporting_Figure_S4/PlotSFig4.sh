6d46439fed618dca5e610fc17ae2255203c76b3a
Christian Holme <christian.holme@med.uni-goettingen.de>
2019-11-25 13:15:59 +0100

    Supporting Figure 4: split reconstruction script
    For reproducibility testing, we only want to use bart, no
    external programs such as python. Therefore, SupFig4.sh
    is now split into a bart-script (RecSFig4.sh) and a
    plot-script (PlotSFig4.sh). The new SupFig4.sh simply
    calls both scripts in succession.

---
+++ Supporting_Figure_S4/PlotSFig4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+# Plot Residual
+sed -n '/Res:/p' Output.txt | awk '{print NR-1, "\t", $0}' | sed 's/Res://' > SupportingFigureS4.txt # Extract Step and Residuum
+# Plot using Python 2.7
+python plot.py -x "Newton step" -y "Residuum [a.u.]" -g -d "0 1" --xlim "-0.5 11.5" -e "eps" --ms 10  SupportingFigureS4.txt SupportingFigureS4
